314236f2da3eb48dcd736d6a56ad851427a6dbae
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-01-28 21:36:58 -0300

    fixed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CpfValidationComponent } from './pages/cpf-validation/cpf-validation.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', title: 'CPF Validation', component: CpfValidationComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
