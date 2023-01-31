e1aa1ed468869e6d5d355fc6d5ca303da4068bb3
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-01-31 19:53:23 -0300

    fixed bottom bar styling and added verification to see if client was found

--- src/app/components/bottombar/bottombar.component.ts
+++ src/app/components/bottombar/bottombar.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bottombar',
   templateUrl: './bottombar.component.html',
-  styleUrls: ['./bottombar.component.scss']
+  styleUrls: ['./bottombar.component.scss'],
 })
 export class BottombarComponent {
-
+  @Input() client: boolean = false;
 }
