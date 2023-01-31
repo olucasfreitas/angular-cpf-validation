e14a63bc8711db7a4bad5b5b04f07cb0951727c9
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-01-31 19:56:12 -0300

    added service to search for cpf

--- src/app/pages/cpf-validation/cpf-validation.component.ts
+++ src/app/pages/cpf-validation/cpf-validation.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Client } from 'src/app/entities/client.entity';
+import { ClientService } from 'src/app/services/client.service';
 
 @Component({
   selector: 'app-cpf-validation',
   templateUrl: './cpf-validation.component.html',
-  styleUrls: ['./cpf-validation.component.scss']
+  styleUrls: ['./cpf-validation.component.scss'],
 })
 export class CpfValidationComponent {
+  client: Client = new Client();
+  clientNotFound = false;
 
+  constructor(private clientService: ClientService) {}
+
+  validateCpf(cpf: string): void {
+    this.clientService.getClientByCPF(cpf).subscribe((client) => {
+      if (client.length === 0) {
+        this.clientNotFound = true;
+      } else {
+        this.clientNotFound = false;
+        Object.assign(this.client, client[0]);
+      }
+    });
+  }
 }
