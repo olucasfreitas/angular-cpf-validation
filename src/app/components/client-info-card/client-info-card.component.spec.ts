5a9189ca70eaf0c33a54c1d7ef01183c0b7a358c
Lucas da Silva Freitas <lucasdasilvafreitas090@gmail.com>
2023-01-31 23:36:22 -0300

    .

--- src/app/components/client-info-card/client-info-card.component.spec.ts
+++ src/app/components/client-info-card/client-info-card.component.spec.ts
@@ -22,11 +22,4 @@ describe('ClientInfoCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  // it('should emit the cpf string if the value is not empty', () => {
-  //   const spy = spyOn(component.cpfEmitter, 'emit');
-  //   component.cpfValue = '12345678901';
-  //   component.onSub();
-  //   expect(spy).toHaveBeenCalledWith('12345678901');
-  // });
 });
